6889020b74154bf7347b64a713c7082f1fbe834a
Guilherme Vinhas <guilherme.vinhas@clieent.com>
2025-02-03 20:44:17 -0300

    initial interface

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.mavenproject1;
 
-/**
- *
- * @author Guilherme J. Vinhas
- */
-public class Mavenproject1 {
+import gui.todo;
 
+public class Mavenproject1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new todo().setVisible(true);
     }
 }
